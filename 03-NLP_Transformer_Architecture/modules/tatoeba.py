34745274b6a0ef0a4e61186128abe1e6c7eb5e77
Yasin Tatar <ytatar9@googlemail.com>
2021-07-02 04:01:56 +0200

    implemented encoder and decoder; training does not work yet

--- 03-NLP_Transformer_Architecture/modules/tatoeba.py
+++ 03-NLP_Transformer_Architecture/modules/tatoeba.py
@@ -31,16 +31,23 @@ class TatoebaDataset(Dataset):
             return None
         
         # get sentences in both languages from line <index>
-        s0, s1 = self.df.loc[index, [self.langs[0] + '_clean', self.langs[1] + '_clean']]
+        if self.debug: print(self.df.loc[index])
+        # s0, s1 = self.df.loc[index, [self.langs[0] + '_clean', self.langs[1] + '_clean']].values
+        s = (self.df.iloc[index, 3:5].values)[0]
+        print(f">> {s=}")
+        s0 = s[0]
+        s1 = s[1]
         
         # vectorize sentences
         if self.debug: print(f" >> {self.langs[0]}: {s0}, {self.langs[1]} {s1}")
         if self.debug: print(f" >> Tokenized: {s0.split(' ')}")
         
-        v0 = [self.t0[s] for s in s0.split(" ") if s != ""]
-        v1 = [self.t1[s] for s in s1.split(" ") if s != ""]
+        v0 = [self.t0[s] for s in s0.split(" ") if s != ""] + [self.t0['EOS']]
+        if self.debug: print(f" >> V0: {v0=}\n")
+        v1 = [self.t1[s] for s in s1.split(" ") if s != ""] + [self.t1['EOS']]
         
         # transform to tensors, .view(-1,1) just ensures that we have 1-d vectors
+        if self.debug: print(f"{v0=}\n{v1=}")
         t0 = torch.tensor(v0).view(-1,1)
         t1 = torch.tensor(v1).view(-1,1)
         
@@ -49,4 +56,7 @@ class TatoebaDataset(Dataset):
     def __len__(self):
         return self.df.shape[0]
         pass
+
+    def vocab_size(self):
+        return self.t0.n_words, self.t1.n_words
     pass
